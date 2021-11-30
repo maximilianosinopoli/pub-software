d7c625d9b12daf6a250dcb892c57190009cbe133
Maximiliano Sinopoli <m_sinopoli@hotmail.com>
2021-11-30 19:15:33 +0000

    objet created

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 const addButtonNav = document.querySelector('#add-new-product')
 const divProduct = document.querySelector('#test')
+const pushNewElement = document.querySelector('#push')
+let productName = document.querySelector('#product-name')
+let productPrice = document.querySelector('#product-price')
+let productQuantity = document.querySelector('#product-quantity')
+let productType = document.querySelector('#product-type')
+const container = document.querySelector('.container')
+
+let products = []
 
 addButtonNav.addEventListener('click', openAddProduct)
+pushNewElement.addEventListener('click', pushNewProduct)
 
 function openAddProduct() {
-    console.log('working')
     divProduct.classList.add('div-add-product-show')
+}
+
+function pushNewProduct() {
+    let newProduct = document.createElement('div');
+    newProduct.classList.add('new-product')
+    let title = document.createElement('h2')
+    title.innerText = productName.value
+    container.appendChild(newProduct)
+    newProduct.appendChild(title)
+    divProduct.classList.remove('div-add-product-show')
+    products.push({
+        name: productName.value,
+        price: productPrice.value,
+        quantity: productQuantity.value,
+        type: productType.value
+    })
+    console.log(products)
 }
