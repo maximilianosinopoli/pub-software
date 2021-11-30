ef85b3fab8e2ac89497c27d4c25605693a02f7ae
Maximiliano Sinopoli <m_sinopoli@hotmail.com>
2021-11-30 18:29:22 +0000

    functionality pop-up add product

--- main.js
+++ main.js
@@ -1,38 +1,9 @@
-let add = document.querySelector('#add')
-let container = document.querySelector('.container')
-let inputAdd = document.querySelector('#input-add')
-let options = document.querySelector('#options')
-let test = document.querySelector('#test')
-let div = document.querySelector('#')
+const addButtonNav = document.querySelector('#add-new-product')
+const divProduct = document.querySelector('#test')
 
-add.addEventListener('click', addElement)
-test.addEventListener('click', showAdd)
+addButtonNav.addEventListener('click', openAddProduct)
 
-function addElement() {
-    let newElement = document.createElement('div');
-    newElement.innerText = inputAdd.value;
-    
-
-    if (options.value === "Beer") {
-        newElement.classList.add('cuadrado-beer');
-    }
-
-    if (options.value === "Snack") {
-        newElement.classList.add('cuadrado-snack');
-    }
-
-    if (options.value === "Soft") {
-        newElement.classList.add('cuadrado-soft');
-    }
-
-    if (options.value === "Spirits") {
-        newElement.classList.add('cuadrado-spirit');
-    }
-    
-    container.appendChild(newElement)
-    console.log(options.value)
-}
-
-function showAdd() {
-    
+function openAddProduct() {
+    console.log('working')
+    divProduct.classList.add('div-add-product-show')
 }
